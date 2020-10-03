953b5770d97a3f721f51fe657680b5f999778eb4
adityadeshmukh11 <adityadeshmukh141111@gmail.com>
2020-10-03 16:25:36 +0530

    function_wise arrangement

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,13 @@
 // }
 
 /* single parquet file reading and writing
-if sample.parquet not exists then it creates a parquet file and writes code from another .parquet file 
+if sample.parquet not exists then it creates a parquet file and writes code from another .parquet file
 (in the form of json).
 if sample.parquet exists then it overrides same file (in the form of json).
+***********************************************main code***********************************************
 */
 
-use std::fs::File;
-use std::path::Path;
 use parquet::file::reader::{FileReader, SerializedFileReader};
-use std::{fs,rc::Rc};
 use parquet::{
     file::{
         properties::WriterProperties,
@@ -34,36 +32,49 @@ use parquet::{
     },
     schema::parser::parse_message_type,
 };
+use std::fs::File;
+use std::path::Path;
+use std::{fs, rc::Rc};
 
-fn main()
+fn reader_parquet()
 {let file = File::open(&Path::new("./parquet/userdata1.parquet")).unwrap();
-let reader = SerializedFileReader::new(file).unwrap();
-let mut iter = reader.get_row_iter(None).unwrap();
-while let Some(record) = iter.next() {
-  println!("{}", record);
+    let reader = SerializedFileReader::new(file).unwrap();
+    let mut iter = reader.get_row_iter(None).unwrap();
+    while let Some(record) = iter.next() {
+        println!("{}", record);
+    }
 }
 
-let path = Path::new("/home/aditya/Desktop/final_wala/hello_cargo/parquet/sample.parquet");
-let message_type = "
+fn writer_parquet()
+{
+    let path = Path::new("/home/aditya/Desktop/final_wala/hello_cargo/parquet/sample.parquet");
+    let message_type = "
   message schema {
     REQUIRED INT32 b;
   }
 ";
-let schema = Rc::new(parse_message_type(message_type).unwrap());
-let props = Rc::new(WriterProperties::builder().build());
-let file = fs::File::create(&path).unwrap();
-let mut writer = SerializedFileWriter::new(file, schema, props).unwrap();
-let mut row_group_writer = writer.next_row_group().unwrap();
-while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-    // ... write values to a column writer
-    row_group_writer.close_column(col_writer).unwrap();
+    // let mut new=0;
+    let schema = Rc::new(parse_message_type(message_type).unwrap());
+    let props = Rc::new(WriterProperties::builder().build());
+    let file = fs::File::create(&path).unwrap();
+    let mut writer = SerializedFileWriter::new(file, schema, props).unwrap();
+    let mut row_group_writer = writer.next_row_group().unwrap();
+    while let Some(col_writer) = row_group_writer.next_column().unwrap() {
+        // ... write values to a column writer
+        row_group_writer.close_column(col_writer).unwrap();
+        // new=new+1;
+    }
+    writer.close_row_group(row_group_writer).unwrap();
+    writer.close().unwrap();
+
+    let bytes = fs::read(&path).unwrap();
+    assert_eq!(&bytes[0..4], &[b'P', b'A', b'R', b'1']);
+    // println!("{}",new);
 }
-writer.close_row_group(row_group_writer).unwrap();
-writer.close().unwrap();
 
-let bytes = fs::read(&path).unwrap();
-assert_eq!(&bytes[0..4], &[b'P', b'A', b'R', b'1']);
-
+fn main() {
+    reader_parquet();
+    writer_parquet();
 }
 
 // ****************//read from json file// ****************//
@@ -78,4 +89,3 @@ assert_eq!(&bytes[0..4], &[b'P', b'A', b'R', b'1']);
 //     let str = &copy(&mut file, &mut stdout).unwrap().to_string();
 //     let _data = Json::from_str(str).unwrap();
 // }
-
