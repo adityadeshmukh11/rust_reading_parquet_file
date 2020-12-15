1daf61ef915de5e46a51b88541e6c6dcfb2c61e5
adityadeshmukh11 <aditya.deshmukh@net-mon.net>
2020-12-15 10:23:24 +0530

    added rust writer function(writing something)

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,19 @@ use std::fs::File;
 use std::path::Path;
 use std::{fs, rc::Rc};
 
+use parquet::column::writer::ColumnWriter;
+use std::error::Error;
+
+// use core::fmt::Error;
+// use crate::fs::fs_imp::Error;
+// use crate::fs::fs_imp::ptr::fmt::Error;
+
+// use crate::fs::io::sys::ext::net::raw_fd::net::parser::Error;
+
+
+
 fn reader_parquet()
-{let file = File::open(&Path::new("./parquet/userdata1.parquet")).unwrap();
+{let file = File::open(&Path::new("/home/aditya/Desktop/final_wala/hello_cargo/parquet/sample.parquet")).unwrap();
     let reader = SerializedFileReader::new(file).unwrap();
     let mut iter = reader.get_row_iter(None).unwrap();
     while let Some(record) = iter.next() {
@@ -49,11 +60,11 @@ fn writer_parquet()
 {
     let path = Path::new("/home/aditya/Desktop/final_wala/hello_cargo/parquet/sample.parquet");
     let message_type = "
-  message schema {
-    REQUIRED INT32 b;
-  }
-";
+    message schema {
+        REQUIRED INT32 b;
+    }";
     // let mut new=0;
+
     let schema = Rc::new(parse_message_type(message_type).unwrap());
     let props = Rc::new(WriterProperties::builder().build());
     let file = fs::File::create(&path).unwrap();
@@ -72,20 +83,53 @@ fn writer_parquet()
     // println!("{}",new);
 }
 
-fn main() {
-    reader_parquet();
-    writer_parquet();
+fn write_parquet() -> Result<(), Box<dyn Error>> {
+
+    // TODO let message_type = build_parquet_schema();
+    let message_type = "
+        message schema {REQUIRED BYTE_ARRAY Id;REQUIRED BYTE_ARRAY Name;REQUIRED INT32 Age;}
+    ";
+    let schema = Rc::new(parse_message_type(message_type)?);
+    let props = Rc::new(WriterProperties::builder().build());
+    let file = File::create("file1.parquet")?;
+    let mut writer = SerializedFileWriter::new(file, schema, props).unwrap();
+    let mut row_group_writer = writer.next_row_group().unwrap();
+    let mut col_number = 0;
+
+    while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
+        col_number = col_number + 1;
+        match col_writer {
+            ColumnWriter::ByteArrayColumnWriter(ref mut typed_writer) => {
+                println!("writing a byte array");
+                // I can remove this if-else when I start taking fn parameters of my schema and columns
+                if col_number == 1 {
+                    typed_writer.write_batch(
+                        &[parquet::data_type::ByteArray::from("123-adf"), parquet::data_type::ByteArray::from("sdf-ge2")], None, None
+                    )?;
+                } else {
+                    typed_writer.write_batch(
+                        &[parquet::data_type::ByteArray::from("John Doe"), parquet::data_type::ByteArray::from("Jane Doe")], None, None
+                    )?;
+                }
+            },
+            ColumnWriter::Int32ColumnWriter(ref mut typed_writer) => {
+                println!("writing an integer");
+                typed_writer.write_batch(
+                    &[25, 26], None, None
+                )?;
+            },
+            _ => {}
+        }
+        row_group_writer.close_column(col_writer)?;
+    }
+    // println!(row_group_writer)
+    writer.close_row_group(row_group_writer)?;
+    writer.close()?;
+    Ok(())
 }
 
-// ****************//read from json file// ****************//
-// use rustc_serialize::json::Json;
-// use std::fs::File;
-// use std::io::copy;
-// use std::io::stdout;
-
-// fn main() {
-//     let mut file = File::open("/home/aditya/Desktop/final_wala/hello_cargo/text.json").unwrap();
-//     let mut stdout = stdout();
-//     let str = &copy(&mut file, &mut stdout).unwrap().to_string();
-//     let _data = Json::from_str(str).unwrap();
-// }
+fn main() {
+    // reader_parquet();
+    // writer_parquet();
+    write_parquet();
+}
